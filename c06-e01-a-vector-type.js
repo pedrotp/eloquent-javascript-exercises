fe98fedf93a7e291f434ed43755fc9f422f78273
pedrotp <pedro@pedrotp.com>
2015-10-24 22:48:24 -0400

    finished chapter 6

---
+++ c06-e01-a-vector-type.js
@@ -0,0 +1,19 @@
+function Vector(x,y) {
+  this.x = x;
+  this.y = y;
+};
+
+Object.defineProperty(Vector.prototype, "plus",
+                      {enumerable: false, value: function(vector) { return new Vector( this.x + vector.x, this.y + vector.y ) } });
+Object.defineProperty(Vector.prototype, "minus",
+                      {enumerable: false, value: function(vector) { return new Vector( this.x - vector.x, this.y - vector.y ) } });
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() { return Math.sqrt((this.x*this.x)+(this.y*this.y)); }
+});
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
