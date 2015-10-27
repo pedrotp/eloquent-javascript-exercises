a83afd057cdc6ed2bb89002b767f7a796eb2afdf
pedrotp <pedro@pedrotp.com>
2015-10-27 10:18:16 -0400

    added c08

---
+++ c08-e02-the-locked-box.js
@@ -0,0 +1,25 @@
+function withBoxUnlocked(body) {
+  if (!box.locked) return body();
+  
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+
+console.log(box.locked);
+// → true
